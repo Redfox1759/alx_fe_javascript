34c87b0e7cd5675b2fe50962dde6732bb3738744
Atokolo John <atokoloj@gmail.com>
2025-06-25 23:06:54 +0100

    Updated file

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -92,3 +92,14 @@ if (savedQuotes) {
 
 
 showRandomQuote();
+
+function importFromJsonFile(event) {
+    const fileReader = new FileReader();
+    fileReader.onload = function(event) {
+      const importedQuotes = JSON.parse(event.target.result);
+      quotes.push(...importedQuotes);
+      saveQuotes();
+      alert('Quotes imported successfully!');
+    };
+    fileReader.readAsText(event.target.files[0]);
+}
